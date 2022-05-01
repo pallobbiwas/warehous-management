43b384c81e050063f960a206f2dc63341d6f3a75
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-01 08:11:46 +0600

    additeam done

--- src/componets/PrivateRoute/AddIteam/AddIteam.js
+++ src/componets/PrivateRoute/AddIteam/AddIteam.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import './AddIteam.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./AddIteam.css";
 
 const AddIteam = () => {
-    return (
-        <div>
-            <h1>add item</h1>
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    fetch("http://localhost:5000/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      });
+  };
+
+  return (
+    <div className="my-5 container-fluid">
+      <h1 className="text-center"> Please add your item here</h1>
+      <hr />
+      <div>
+        <div className="text-center my-5">
+          <form className="w-50 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className="w-100"
+              {...register("name", { required: true })}
+              placeholder="produc name"
+            />
+            <br />
+            <input
+              className="my-3 w-100"
+              {...register("email", { required: true })}
+              placeholder="email"
+            />
+            <br />
+            <input
+              className="w-100"
+              {...register("discription")}
+              placeholder="discription"
+            />
+            <br />
+            <input
+              className="my-3 w-100"
+              {...register("img")}
+              placeholder="img url"
+            />
+            <br />
+            <input
+              className="w-100"
+              {...register("quantity")}
+              placeholder="quantity"
+            />
+            <br />
+            <input
+              className="my-3 w-100"
+              {...register("seller")}
+              placeholder="seller name"
+            />
+            <br />
+            <input
+              className="w-100"
+              type="number"
+              {...register("price")}
+              placeholder="price"
+            />
+            <br />
+            <input className="my-3 w-50" type="submit" />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddIteam;
