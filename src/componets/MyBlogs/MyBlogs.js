07462827294a475eedc941c54807660262fc0c63
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-02 21:29:46 +0600

    finished

--- src/componets/MyBlogs/MyBlogs.js
+++ src/componets/MyBlogs/MyBlogs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 import "./MyBlogs.css";
 
 const MyBlogs = () => {
@@ -14,45 +15,79 @@ const MyBlogs = () => {
       >
         <div className="col-md-6">
           <div className="box">
-            <h4>Difference between javascript and nodejs</h4>
+            <h4 className = "mb-4">Q1. Difference between javascript and nodejs</h4>
             <p>
               <span className="fs-5 fw-bold">Ans: </span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis pariatur animi magnam. Harum deleniti animi, cupiditate
-              delectus similique dolore sint!
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Javascript</th>
+                    <th>Nodejs</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      NodeJS is a cross-platform and opensource Javascript
+                      runtime environment that allows the javascript to be run
+                      on the server-side
+                    </td>
+                    <td>
+                      Javascript is a Scripting language. It is mostly
+                      abbreviated as JS. It can be said that Javascript is the
+                      updated version of the ECMA script.
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
             </p>
           </div>
         </div>
         <div className="col-md-6">
           <div className="box">
-            <h4>When should you use nodejs and when should you use mongodb</h4>
+            <h4 className = "mb-4">
+              Q2. When should you use nodejs and when should you use mongodb
+            </h4>
             <p>
               <span className="fs-5 fw-bold">Ans: </span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis pariatur animi magnam. Harum deleniti animi, cupiditate
-              delectus similique dolore sint!
+              Nodejs: nodejs is a cross-platform and opensource Javascript. It
+              use for connect database with claint site.
+              <br />
+              Mongodb: mongodb is a cloud database. It use for stroge data. and
+              create api
             </p>
           </div>
         </div>
         <div className="col-md-6">
           <div className="box">
-            <h4>Differences between sql and nosql databases</h4>
+            <h4 className = "mb-4">Q3. Differences between sql and nosql databases</h4>
             <p>
               <span className="fs-5 fw-bold">Ans: </span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis pariatur animi magnam. Harum deleniti animi, cupiditate
-              delectus similique dolore sint!
+              <thead>
+                <tr>
+                  <th>Sql</th>
+                  <th>Nosql</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                  SQL databases are vertically scalable. It is table based database system. It is better for multi-row transactions
+                  </td>
+                  <td>
+                  NoSQL databases are horizontally scalable. It is document base database system
+                  </td>
+                </tr>
+              </tbody>
             </p>
           </div>
         </div>
         <div className="col-md-6">
           <div className="box">
-            <h4>What is the purpose of jwt and how does it work</h4>
+            <h4 className = "mb-4">Q4. What is the purpose of jwt and how does it work</h4>
             <p>
               <span className="fs-5 fw-bold">Ans: </span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis pariatur animi magnam. Harum deleniti animi, cupiditate
-              delectus similique dolore sint!
+              Jwt mean javascript wev token. It is a secret key. It create encrypted api.Where unauthorized person can not access api in any way.
             </p>
           </div>
         </div>
